1c5e7d5d91e6299f9d660b2c826dc84cfcc81836
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-04-22 06:28:11 +0200

    Problem without Strategy DP

---
+++ StrategyDP/Program.cs
@@ -0,0 +1,25 @@
+﻿using StrategyDP.With_No_DP;
+
+namespace StrategyDP
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			RealDuck realDuck = new RealDuck();
+			realDuck.Display();
+			realDuck.Fly();
+			realDuck.Quack();
+			realDuck.Swim();
+
+			Console.WriteLine("====================================");
+
+			RubberDuck rubberDuck = new RubberDuck();
+			rubberDuck.Display();
+			rubberDuck.Fly();
+			rubberDuck.Quack();
+			rubberDuck.Swim();
+		}
+	}
+}
---
+++ StrategyDP/With No DP/Duck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDP.With_No_DP
+{
+	public abstract class Duck
+	{
+		public abstract void Display();
+		
+		public void Quack()
+		{
+			Console.WriteLine("Quack");
+		}
+		
+		public void Swim()
+		{
+			Console.WriteLine("All ducks float, even decoys!");
+		}
+
+		public void Fly()
+		{
+			Console.WriteLine("I'm flying!");
+		}
+
+	}
+}
