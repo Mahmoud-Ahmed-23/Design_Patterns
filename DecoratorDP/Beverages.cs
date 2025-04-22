6d10c030c92663510a39b7311bcfb258c0d9644a
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-04-23 01:55:08 +0200

    Structural Patterns - Decorator DP

---
+++ DecoratorDP/Beverages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorDP
+{
+	abstract class Beverage
+	{
+		public virtual string Description { get; set; }
+		public virtual decimal Cost { get; set; }
+		public override string ToString()
+		{
+			return $"{Description} : {Cost:c}";
+		}
+	}
+
+	class Espresso : Beverage
+	{
+		public Espresso()
+		{
+			Description = "Espresso";
+			Cost = 1.99m;
+		}
+	}
+	class HouseBlend : Beverage
+	{
+		public HouseBlend()
+		{
+			Description = "House Blend Coffee";
+			Cost = 0.89m;
+		}
+	}
+	class DarkRoast : Beverage
+	{
+		public DarkRoast()
+		{
+			Description = "Dark Roast Coffee";
+			Cost = 0.99m;
+		}
+	}
+
+
+}
---
+++ DecoratorDP/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DecoratorDP
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Beverage beverage = new Espresso();
+			Console.WriteLine(beverage);
+			Console.WriteLine("======================================");
+
+			beverage = new Milk(beverage);
+			Console.WriteLine(beverage);
+			Console.WriteLine("======================================");
+
+			beverage = new Mocha(beverage);
+			Console.WriteLine(beverage);
+			Console.WriteLine("======================================");
+
+			beverage = new Caramel(beverage);
+			Console.WriteLine(beverage);
+			Console.WriteLine("======================================");
+
+			Beverage beverage2 = new HouseBlend();
+			Console.WriteLine(beverage2);
+			Console.WriteLine("======================================");
+
+			beverage2 = new Milk(beverage2);
+			Console.WriteLine(beverage2);
+			Console.WriteLine("======================================");
+
+			beverage2 = new Mocha(beverage2);
+			Console.WriteLine(beverage2);
+			Console.WriteLine("======================================");
+		}
+	}
+}
